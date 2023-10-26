e517895c9b3fd3a8a4a2fe76ab0c7eec9a1aed77
rubenpilca70 <rubenpilca70@gmail.com>
2023-10-26 10:24:29 -0500

    vista enfermeria

---
+++ vista/ENFERMERIA/Cursos/registrar_paralelo.php
@@ -0,0 +1,79 @@
+<div class="page-wrapper">
+  <div class="page-content">
+
+    <!--breadcrumb-->
+    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+      <div class="breadcrumb-title pe-3">Enfermería</div>
+      <?php
+      // print_r($_SESSION['INICIO']);die();
+
+      ?>
+      <div class="ps-3">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb mb-0 p-0">
+            <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">Registrar Paralelo</li>
+          </ol>
+        </nav>
+      </div>
+    </div>
+    <!--end breadcrumb-->
+
+    <div class="row">
+      <div class="col-xl-12 mx-auto">
+        <div class="card border-top border-0 border-4 border-primary">
+          <div class="card-body p-5">
+            <div class="card-title d-flex align-items-center">
+              <div><i class="bx bxs-user me-1 font-22 text-primary"></i>
+              </div>
+              <h5 class="mb-0 text-primary">Registrar Paralelo</h5>
+            </div>
+            <hr>
+
+            <form action="" method="post">
+
+              <div class="row pt-3">
+                <div class="col-md-6">
+                  <label for="" class="form-label">Sección: <label style="color: red;">*</label> </label>
+                  <select class="form-select" id="" name="">
+                    <option selected>-- Seleccione --</option>
+                    <option value="">Educacion General Basica</option>
+                    <option value="">Bachillerato General Unificado</option>
+                  </select>
+                </div>
+              </div>
+
+              <div class="row pt-3">
+                <div class="col-md-6">
+                  <label for="" class="form-label">Grado: <label style="color: red;">*</label> </label>
+                  <select class="form-select" id="" name="">
+                    <option selected>-- Seleccione --</option>
+                    <option value="">Primer Grado</option>
+                    <option value="">Segundo Grado</option>
+                  </select>
+                </div>
+              </div>
+
+              <div class="row pt-3">
+                <div class="col-md-6">
+                  <label for="" class="form-label">Paralelo: <label style="color: red;">*</label> </label>
+                  <input type="text" class="form-control" id="" name="">
+                </div>
+              </div>
+
+              <div class="col-12 pt-4">
+                <button type="submit" class="btn btn-primary px-5">Guardar</button>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!--plugins-->
+
+<!--app JS-->
+<!-- <script src="assets/js/app.js"></script> -->
