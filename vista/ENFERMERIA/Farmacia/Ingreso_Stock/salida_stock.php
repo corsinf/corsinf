2b6e879fd3921c9b746c8ac90e97dca4a0b23d59
javo <javier.farinango92@gmail.com>
2024-02-21 11:50:49 -0500

    cambio vista

--- vista/ENFERMERIA/Farmacia/Ingreso_Stock/salida_stock.php
+++ vista/ENFERMERIA/Farmacia/Ingreso_Stock/salida_stock.php
@@ -439,7 +439,7 @@ tablaInsu = $('#tabla_insumos').DataTable({
                     <ol class="breadcrumb mb-0 p-0">
                         <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
                         </li>
-                        <li class="breadcrumb-item active" aria-current="page">Insumos</li>
+                        <li class="breadcrumb-item active" aria-current="page">Salida de Medicamentos</li>
                     </ol>
                 </nav>
             </div>
