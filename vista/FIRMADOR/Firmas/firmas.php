d515ff54b9c659eb4952aa78f0d287b20461e4a7
javo <javier.farinango92@gmail.com>
2024-07-15 10:22:32 -0500

    primera reunion

--- vista/FIRMADOR/Firmas/firmas.php
+++ vista/FIRMADOR/Firmas/firmas.php
@@ -1,3 +1,53 @@
-<?php
+<div class="page-wrapper">
+    <div class="page-content">
+        <!--breadcrumb-->
+        <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+            <div class="breadcrumb-title pe-3">Firmador</div>
+            <?php
+            // print_r($_SESSION['INICIO']);die();
 
-echo 'Aqui van las firmas';
+            ?>
+            <div class="ps-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb mb-0 p-0">
+                        <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">
+                        Firmador
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+        <!--end breadcrumb-->
+
+        <div>
+            <?php //print_r($_SESSION) ?>
+        </div>
+
+        <div class="row">
+            <div class="col-xl-12 mx-auto">
+                <div class="card border-top border-0 border-4 border-primary">
+                    <div class="card-body p-5">
+                        <div class="card-title d-flex align-items-center">
+
+                            <h5 class="mb-0 text-primary"></h5>
+
+                            <div class="row mx-0">
+                                <div class="col-sm-12" id="btn_nuevo">
+
+                                    <button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#modal_pacientes"><i class="bx bx-plus"></i> Nuevo Paciente</button>
+
+                                </div>
+                            </div>
+                        </div>
+
+
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--end row-->
+    </div>
+</div>
