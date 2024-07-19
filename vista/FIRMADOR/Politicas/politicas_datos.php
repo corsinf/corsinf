eef07b658cba9a2c2a19615c7c3f4b50daf27c36
Adrian Acuña <adriansamacu71@gmail.com>
2024-07-19 17:32:15 -0500

    Pendiente página de políticas

---
+++ vista/FIRMADOR/Politicas/politicas_datos.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Políticas de Privacidad</title>
+</head>
+
+<body>
+    <!--wrapper-->
+    <div class="wrapper">
+        <!--sidebar wrapper -->
+        <div class="sidebar-wrapper" data-simplebar="true">
+            <div class="sidebar-header">
+                <div>
+                    <h4 class="logo-text">Políticas de Privacidad</h4>
+                </div>
+                <div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
+                </div>
+            </div>
+            <!--navigation-->
+            <ul class="metismenu" id="menu">
+                <li>
+                    <a href="javascript:;">
+                        <div class="parent-icon"><i class='bx bx-home-circle'></i>
+                        </div>
+                        <div class="menu-title"><a href="">Introducción</a></div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript:;">
+                        <div class="parent-icon"><i class='bx bx-home-circle'></i>
+                        </div>
+                        <div class="menu-title">Información que recopilamos</div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript:;">
+                        <div class="parent-icon"><i class='bx bx-home-circle'></i>
+                        </div>
+                        <div class="menu-title">Uso de su información</div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript:;">
+                        <div class="parent-icon"><i class='bx bx-home-circle'></i>
+                        </div>
+                        <div class="menu-title">Tiempo de conservación de los datos</div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript:;">
+                        <div class="parent-icon"><i class='bx bx-home-circle'></i>
+                        </div>
+                        <div class="menu-title">Derechos de los usuarios</div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript:;">
+                        <div class="parent-icon"><i class='bx bx-home-circle'></i>
+                        </div>
+                        <div class="menu-title">Seguridad de los Datos</div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript:;">
+                        <div class="parent-icon"><i class='bx bx-home-circle'></i>
+                        </div>
+                        <div class="menu-title">Uso de cookies</div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript:;">
+                        <div class="parent-icon"><i class='bx bx-home-circle'></i>
+                        </div>
+                        <div class="menu-title">Compartir información con terceros</div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript:;">
+                        <div class="parent-icon"><i class='bx bx-home-circle'></i>
+                        </div>
+                        <div class="menu-title">Contacto</div>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+    <!--start page wrapper -->
+    <div class="page-wrapper">
+        <div class="page-content">
+            <!--breadcrumb-->
+            <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+                <div class="breadcrumb-title pe-3">Política de Privacidad</div>
+                <div class="ps-3">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb mb-0 p-0">
+                            <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                            </li>
+                            <li class="breadcrumb-item active" aria-current="page">Introducción</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+            <!--end breadcrumb-->
+            <div class="row">
+                <div class="col-12 col-lg-8">
+                    <div class="card">
+                        <div class="card-body">
+                                <div>
+                                    <h5 class="mb-3">Introducción</h5>
+                                    <p class="mb-4">En Corsinf valoramos su privacidad y nos comprometemos a proteger sus datos personales. Esta política de privacidad describe cómo recopilamos, usamos, almacenamos y protegemos su información. También te informamos cuáles son tus derechos. Esta política de privacidad rige para toda la información personal que recibe Corsinf sobre personas que no son empleados de Corsinf.</p>
+                                </div>
+                                <div>
+                                    <h5 class="mb-3">Información que recopilamos</h5>
+                                    <p class="">Recopilamos los siguientes tipos de información: </p>
+                                    <ul class="list-unstyled mb-4">
+                                        <li class="mb-2"><strong>Datos de identificación: </strong>nombre, dirección, número de teléfono.</li>
+                                        <li class="mb-2"><strong>Datos de inicio de sesión: </strong>correo electrónico, contraseña, información de la computadora y la conexión.</li>
+                                        <li class="mb-2"><strong>Datos de uso: </strong>información sobre cómo utiliza nuestros servicios y productos.</li>
+                                        <li class="mb-2"><strong>Datos de preferencias: </strong>información sobre sus intereses y preferencias. </li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h5 class="mb-3">Uso de su información</h5>
+                                    <p class="mb-3">Usamos su información para los siguientes propósitos:</p>
+                                    <div class="list-group mb-4">
+                                        <a href="" class="list-group-item list-group-item-action">Proveer y gestionar nuestros servicios</a>
+                                        <a href="" class="list-group-item list-group-item-action">Proveer y gestionar nuestros servicios</a>
+                                    </div>
+                                    <p class="">En Corsinf estamos comprometidos en cumplir todas las obligaciones legales y contractuales con el objetivo de garantizar la seguridad de su información mientras nos mantenemos bajo el marco legal del país.</p>
+                                </div>
+                                <div>
+                                    <h5 class="mb-3">Tiempos de conservación de los datos</h5>
+                                    <p class="mb-3">Retenemos sus datos personales el tiempo necesario para cumplir con los propósitos para los que se recopilaron, incluyendo el cumplimiento de obligaciones legales, contables o de informes. Los criterios utilizados para determinar los períodos de retención incluyen:</p>
+                                    <ul class="list-unstyled mb-4">
+                                        <li class="mb-2">El tiempo durante el cual mantenemos una relación continua con usted y le proporcionamos nuestros servicios.</li>
+                                        <li class="mb-2">La existencia de una obligación legal a la que estemos sujetos.</li>
+                                        <li class="mb-2">El período de tiempo en el que se pueda presentar una reclamación contra nosotros.</li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h5 class="mb-3">Derechos de los usuarios </h5>
+                                    <p class="mb-3">De acuerdo con la Ley Orgánica de Protección de Datos Personales, usted tiene los siguientes derechos:</p>
+                                    <ul class="list-unstyled mb-4">
+                                        <li class="mb-2"><strong>Acceso: </strong>Puede solicitar una copia de sus datos personales que tenemos.</li>
+                                        <li class="mb-2"><strong>Rectificación y actualización: </strong>uede solicitar la corrección de cualquier dato incorrecto o incompleto.</li>
+                                        <li class="mb-2"><strong>Eliminación: </strong>Puede solicitar la eliminación de sus datos personales cuando ya no sean necesarios para los fines para los que fueron recopilados.</li>
+                                        <li class="mb-2"><strong>Oposición: </strong>Puede oponerse al tratamiento de sus datos personales por motivos relacionados con su situación particular.</li>
+                                        <li class="mb-2"><strong>Portabilidad: </strong>Puede solicitar que transfiramos sus datos a otra organización o a usted en un formato estructurado, de uso común y lectura mecánica.</li>
+                                        <li class="mb-2"><strong>Suspensión: </strong>Puede solicitar la suspensión del tratamiento de sus datos.</li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <h5 class="mb-3">Responsabilidades del usuario </h5>
+                                    <p class="mb-3">En Corsinf respetamos y protegemos la privacidad de los datos personales de nuestros usuarios. En este punto se plantea todas las limitaciones que tienen las personas para usar, transmitir y divulgar los servicio e información de Corsinf. Para garantizar una experiencia segura y conforme a la normativa, pedimos a nuestros usuarios cumplan con las siguientes responsabilidades:</p>
+                                    <div class="list-group mb-4">
+                                        <a href="" class="list-group-item list-group-item-action">Proveer información veraz y actualizada.</a>
+                                        <a href="" class="list-group-item list-group-item-action">Protección de sus credenciales.</a>
+                                        <a href="" class="list-group-item list-group-item-action">Cumplimiento con la normativa de protección de datos.</a>
+                                        <a href="" class="list-group-item list-group-item-action">Notificación de incidentes.</a>
+                                        <a href="" class="list-group-item list-group-item-action">Uso adecuado de los servicios.</a>
+                                        <a href="" class="list-group-item list-group-item-action">Derechos de los titulares de datos.</a>
+                                    </div>
+                                </div>
+                                <div>
+                                    <h5 class="mb-3">Seguridad de los Datos</h5>
+                                    <p class="mb-4">Implementamos medidas razonables para garantizar la seguridad de sus datos personales y protegerlos de pérdidas, usos indebidos, el acceso no autorizado, la alteración, divulgación y destrucción. Corsinf ha implementado los procedimientos necesarios para asegurar y proteger su información.</p>
+                                </div>
+                                <div>
+                                    <h5 class="mb-3">Uso de cookies</h5>
+                                    <p class="mb-4">Mientras navega en corsinf.com, utilizamos cookies y tecnologías similares para mejorar su experiencia en nuestro sitio web, analizar el uso del sitio y ofrecer contenido personalizado. Puede configurar su navegador para rechazar cookies o recibir una notificación cuando se envíen. Sin embargo, algunas características de nuestro sitio pueden no funcionar correctamente sin cookies.</p>
+                                </div>
+                                <div>
+                                    <h5 class="mb-3">Compartir información con terceros</h5>
+                                    <p class="mb-4">Podemos compartir su información con socios y proveedores de servicios terceros que nos ayudan a operar nuestro negocio, tales como procesamiento de pagos y servicios de marketing. Estos proveedores están obligados a proteger su información y solo pueden usarla para los fines específicos para los cuales fue compartida.</p>
+                                </div>
+                                <div>
+                                    <h5 class="mb-3">Contacto</h5>
+                                    <p class="mb-4">Si tiene preguntas o inquietudes sobre nuestra política de privacidad o sobre el manejo de sus datos personales, puede contactarnos a través de: </p>
+                                    <ul class="list-unstyled mb-4">
+                                        <li class="mb-2"><strong>Correo electrónico: </strong>contacto@corsinf.com</li>
+                                        <li class="mb-2"><strong>Teléfono: </strong>(+593) 99-921-9738</li>
+                                        <li class="mb-2"><strong>Dirección: </strong>De los Motilones, N40-345. Quito, Ecuador</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--end row-->
+        </div>
+    </div>
+    <!--end page wrapper -->
+</body>
+
+</html>
+
+<!--<a href="" class="list-group-item list-group-item-action"></a>-->
