9085d3077aa320d5c3515dc89761e7e0b286031e
jayelsea <fuirner@gmail.com>
2024-08-13 11:59:27 -0500

    Revisión

--- vista/INICIO_MODULOS/mod_pasantes_1.php
+++ vista/INICIO_MODULOS/mod_pasantes_1.php
@@ -2,7 +2,7 @@
 if ($_GET['acc'] == 'registro_pasantes_fin') {
     include('FIRMADOR/PASANTES/registro_pasantes_fin.php');
 }
-if ($_GET['acc'] == 'Luis') {
+if ($_GET['acc'] == 'crear_oficina') {
     include('COWORKING/crear_oficina.php');
 }
 ?>
