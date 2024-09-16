274204fa7b110e53a5ead57f15a4027be77048b3
Alee <apbarrionuevo@puce.edu.ec>
2024-09-16 15:26:39 -0500

    PDFS LISTOS

--- vista/COWORKING/crear_mienbros.php
+++ vista/COWORKING/crear_mienbros.php
@@ -344,7 +344,7 @@
                     confirmButtonText: 'Aceptar'
                 });
             }
-        },
+        }
         error: function() {
             Swal.fire({
                 title: 'Error',
@@ -562,28 +562,15 @@
         });
     }
 
-        function generarExcelCompras() {
-            const ws = XLSX.utils.table_to_sheet(document.getElementById('tbl_compras'));
-            const wb = XLSX.utils.book_new();
-            XLSX.utils.book_append_sheet(wb, ws, "Compras");
-            XLSX.writeFile(wb, "compras.xlsx");
+
+
+        function generarPDFMiembros() {
+            var url ='../controlador/COWORKING/crear_mienbrosC.php?generarPDFMiembros=true'
+            window.open(url,"_blank");
         }
 
-        async function generarPDFMiembros() {
-            const { jsPDF } = window.jspdf;
-            const doc = new jsPDF();
-            doc.text('Informe de Miembros', 10, 10);
-            const table = document.getElementById('tbl_miembros');
-            doc.autoTable({ html: table });
-            doc.save('miembros.pdf');
-        }
-
-        async function generarPDFCompras() {
-            const { jsPDF } = window.jspdf;
-            const doc = new jsPDF();
-            doc.text('Informe de Compras', 10, 10);
-            const table = document.getElementById('tbl_compras');
-            doc.autoTable({ html: table });
-            doc.save('compras.pdf');
+        function generarPDFCompras() {
+            var url ='../controlador/COWORKING/crear_mienbrosC.php?generarPDFCompras=true'
+            window.open(url,"_blank");
         }
 </script>
