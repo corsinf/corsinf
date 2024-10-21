f7fa6c31b1387b30fd0b21a5afc370147f6ef29c
Alee <apbarrionuevo@puce.edu.ec>
2024-10-21 09:10:53 -0500

    listo

--- vista/COWORKING/crear_mienbrosdos.php
+++ vista/COWORKING/crear_mienbrosdos.php
@@ -1,27 +1,149 @@
-<div class="container my-4">
-        <div class="d-flex justify-content-between">
-            <button class="btn btn-primary mb-3">+ Nuevo Producto</button>
-            <div class="input-group mb-3" style="width: 300px;">
-                <input type="text" class="form-control" placeholder="Buscar producto...">
-                <button class="btn btn-outline-secondary" type="button">Buscar</button>
-            </div>
-        </div>
-        <div class="row">
-            <!-- Repetir este bloque para cada producto -->
-            <div class="col-md-3">
-                <div class="product-card position-relative">
-                    <div class="discount-badge">-10%</div>
-                    <img src="https://via.placeholder.com/200" alt="Producto 1">
-                    <div class="product-info">
-                        <h5 class="text-center">Nest Shaped Chair</h5>
-                        <p class="text-center">134 Ventas</p>
-                        <p class="text-center"><del>$350</del> <strong>$240</strong></p>
-                        <div class="text-center">
-                            <span class="badge bg-warning text-dark">4.2 (182)</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Fin del bloque de producto -->
-        </div>
-    </div>
+<div class="wrapper">
+		<!--sidebar wrapper -->
+		<div class="sidebar-wrapper" data-simplebar="true">
+			<div class="sidebar-header">
+				<div>
+					<img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
+				</div>
+				<div>
+					<h4 class="logo-text">Syndron</h4>
+				</div>
+				<div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
+				</div>
+			</div>
+			<!--navigation-->
+			<ul class="metismenu" id="menu">
+				<li>
+					<a href="javascript:;" class="has-arrow">
+						<div class="parent-icon"><i class='bx bx-home-circle'></i>
+						</div>
+						<div class="menu-title">Dashboard</div>
+					</a>
+					<ul>
+						<li> <a href="index.html"><i class="bx bx-right-arrow-alt"></i>eCommerce</a>
+						</li>
+						<li> <a href="index2.html"><i class="bx bx-right-arrow-alt"></i>Analytics</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="javascript:;" class="has-arrow">
+						<div class="parent-icon"><i class="bx bx-category"></i>
+						</div>
+						<div class="menu-title">Application</div>
+					</a>
+					<ul>
+						<li> <a href="app-emailbox.html"><i class="bx bx-right-arrow-alt"></i>Email</a>
+						</li>
+						<li> <a href="app-chat-box.html"><i class="bx bx-right-arrow-alt"></i>Chat Box</a>
+						</li>
+						<li> <a href="app-file-manager.html"><i class="bx bx-right-arrow-alt"></i>File Manager</a>
+						</li>
+						<li> <a href="app-contact-list.html"><i class="bx bx-right-arrow-alt"></i>Contatcs</a>
+						</li>
+						<li> <a href="app-to-do.html"><i class="bx bx-right-arrow-alt"></i>Todo List</a>
+						</li>
+						<li> <a href="app-invoice.html"><i class="bx bx-right-arrow-alt"></i>Invoice</a>
+						</li>
+						<li> <a href="app-fullcalender.html"><i class="bx bx-right-arrow-alt"></i>Calendar</a>
+						</li>
+					</ul>
+				</li>
+				<li class="menu-label">UI Elements</li>
+				<li>
+					<a href="widgets.html">
+						<div class="parent-icon"><i class='bx bx-cookie'></i>
+						</div>
+						<div class="menu-title">Widgets</div>
+					</a>
+				</li>
+				<li>
+					<a href="javascript:;" class="has-arrow">
+						<div class="parent-icon"><i class='bx bx-cart'></i>
+						</div>
+						<div class="menu-title">eCommerce</div>
+					</a>
+					<ul>
+						<li> <a href="ecommerce-products.html"><i class="bx bx-right-arrow-alt"></i>Products</a>
+						</li>
+						<li> <a href="ecommerce-products-details.html"><i class="bx bx-right-arrow-alt"></i>Product Details</a>
+						</li>
+						<li> <a href="ecommerce-add-new-products.html"><i class="bx bx-right-arrow-alt"></i>Add New Products</a>
+						</li>
+						<li> <a href="ecommerce-orders.html"><i class="bx bx-right-arrow-alt"></i>Orders</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a class="has-arrow" href="javascript:;">
+						<div class="parent-icon"><i class='bx bx-bookmark-heart'></i>
+						</div>
+						<div class="menu-title">Components</div>
+					</a>
+					<ul>
+						<li> <a href="component-alerts.html"><i class="bx bx-right-arrow-alt"></i>Alerts</a>
+						</li>
+						<li> <a href="component-accordions.html"><i class="bx bx-right-arrow-alt"></i>Accordions</a>
+						</li>
+						<li> <a href="component-badges.html"><i class="bx bx-right-arrow-alt"></i>Badges</a>
+						</li>
+						<li> <a href="component-buttons.html"><i class="bx bx-right-arrow-alt"></i>Buttons</a>
+						</li>
+						<li> <a href="component-cards.html"><i class="bx bx-right-arrow-alt"></i>Cards</a>
+						</li>
+						<li> <a href="component-carousels.html"><i class="bx bx-right-arrow-alt"></i>Carousels</a>
+						</li>
+						<li> <a href="component-list-groups.html"><i class="bx bx-right-arrow-alt"></i>List Groups</a>
+						</li>
+						<li> <a href="component-media-object.html"><i class="bx bx-right-arrow-alt"></i>Media Objects</a>
+						</li>
+						<li> <a href="component-modals.html"><i class="bx bx-right-arrow-alt"></i>Modals</a>
+						</li>
+						<li> <a href="component-navs-tabs.html"><i class="bx bx-right-arrow-alt"></i>Navs & Tabs</a>
+						</li>
+						<li> <a href="component-navbar.html"><i class="bx bx-right-arrow-alt"></i>Navbar</a>
+						</li>
+						<li> <a href="component-paginations.html"><i class="bx bx-right-arrow-alt"></i>Pagination</a>
+						</li>
+						<li> <a href="component-popovers-tooltips.html"><i class="bx bx-right-arrow-alt"></i>Popovers & Tooltips</a>
+						</li>
+						<li> <a href="component-progress-bars.html"><i class="bx bx-right-arrow-alt"></i>Progress</a>
+						</li>
+						<li> <a href="component-spinners.html"><i class="bx bx-right-arrow-alt"></i>Spinners</a>
+						</li>
+						<li> <a href="component-notifications.html"><i class="bx bx-right-arrow-alt"></i>Notifications</a>
+						</li>
+						<li> <a href="component-avtars-chips.html"><i class="bx bx-right-arrow-alt"></i>Avatrs & Chips</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a class="has-arrow" href="javascript:;">
+						<div class="parent-icon"><i class="bx bx-repeat"></i>
+						</div>
+						<div class="menu-title">Content</div>
+					</a>
+					<ul>
+						<li> <a href="content-grid-system.html"><i class="bx bx-right-arrow-alt"></i>Grid System</a>
+						</li>
+						<li> <a href="content-typography.html"><i class="bx bx-right-arrow-alt"></i>Typography</a>
+						</li>
+						<li> <a href="content-text-utilities.html"><i class="bx bx-right-arrow-alt"></i>Text Utilities</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a class="has-arrow" href="javascript:;">
+						<div class="parent-icon"> <i class="bx bx-donate-blood"></i>
+						</div>
+						<div class="menu-title">Icons</div>
+					</a>
+					<ul>
+						<li> <a href="icons-line-icons.html"><i class="bx bx-right-arrow-alt"></i>Line Icons</a>
+						</li>
+						<li> <a href="icons-boxicons.html"><i class="bx bx-right-arrow-alt"></i>Boxicons</a>
+						</li>
+						<li> <a href="icons-feather-icons.html"><i class="bx bx-right-arrow-alt"></i>Feather Icons</a>
+						</li>
+					</ul>
+				</li>
