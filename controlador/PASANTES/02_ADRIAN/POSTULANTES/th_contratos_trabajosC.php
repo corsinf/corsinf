7ec09d1751e5f7cae258dc36dc491a94e364b98d
MarcoAntonio096 <magomezv@puce.edu.ec>
2024-11-21 13:25:52 -0500

    contrato de trabajo

---
+++ controlador/PASANTES/02_ADRIAN/POSTULANTES/th_contratos_trabajosC.php
@@ -0,0 +1,193 @@
+<?php
+
+require_once(dirname(__DIR__, 4) . '/modelo/PASANTES/02_ADRIAN/POSTULANTES/th_contratos_trabajosM.php');
+
+$controlador = new th_contratos_trabajosC();
+
+if (isset($_GET['listar'])) {
+    echo json_encode($controlador->listar($_POST['id']));
+}
+
+if (isset($_GET['listar_modal'])) {
+    echo json_encode($controlador->listar_modal($_POST['id']));
+}
+
+if (isset($_GET['insertar'])) {
+    echo json_encode($controlador->insertar_editar($_FILES, $_POST));
+}
+
+if (isset($_GET['eliminar'])) {
+    echo json_encode($controlador->eliminar($_POST['id']));
+}
+
+class th_contratos_trabajosC
+{
+    private $modelo;
+
+    function __construct()
+    {
+        $this->modelo = new th_contratos_trabajosM();
+    }
+
+    //Funcion para listar la formacion academica del postulante
+    function listar($id)
+    {
+        $datos = $this->modelo->where('th_pos_id', $id)->where('th_ctr_estado', 1)->listar();
+
+        $texto = '';
+        foreach ($datos as $key => $value) {
+            $url_pdf = '../REPOSITORIO/TALENTO_HUMANO.pdf';
+
+            $texto .=
+                <<<HTML
+                    <div class="row mb-3">
+                        <div class="col-10">
+                            <p class="fw-bold my-0 d-flex align-items-center">{$value['th_ctr_nombre_empresa']}</p>
+                            <a href="#" data-bs-toggle="modal" data-bs-target="#modal_ver_pdf_contratos" onclick="definir_ruta_iframe_contratos('{$value['th_ctr_ruta_archivo']}');">Ver Certifiacado o Capacitacion PDF</a>
+                        </div>
+                        <div class="col-2 d-flex justify-content-end align-items-center">
+                            <button class="btn btn-xs" style="color: white;" onclick="abrir_modal_contratos_trabajos('{$value['_id']}')">
+                                <i class="text-dark bx bx-pencil me-0" style="font-size: 20px;"></i>
+                            </button>
+                        </div>
+                    </div>
+                HTML;
+        }
+
+        return $texto;
+    }
+
+     //Buscando registros por id de la formacion academica
+     function listar_modal($id)
+     {
+         if ($id == '') {
+             $datos = $this->modelo->where('th_ctr_estado', 1)->listar();
+         } else {
+             $datos = $this->modelo->where('th_ctr_id', $id)->listar();
+         }
+         return $datos;
+     }
+ 
+     function insertar_editar($file, $parametros)
+     {
+         // print_r($file);
+         // exit();
+         // die();
+         $datos = array(            
+             array('campo' => 'th_ctr_nombre_empresa', 'dato' => $parametros['txt_nombre_empresa_contrato']),
+             array('campo' => 'th_ctr_tipo_contrato', 'dato' => $parametros['txt_tipo_contrato']),
+             array('campo' => 'th_ctr_fecha_inicio_contrato', 'dato' => $parametros['txt_fecha_inicio_contrato']),
+             array('campo' => 'th_ctr_fecha_fin_contrato', 'dato' => $parametros['txt_fecha_fin_contrato']),
+             array('campo' => 'th_pos_id', 'dato' => $parametros['txt_postulante_id']),
+         );
+ 
+ 
+         $txt_id_contratos_trabajos = $parametros['txt_contratos_trabajos_id'];
+ 
+         if ($txt_id_contratos_trabajos == '') {
+             $datos = $this->modelo->insertar_id($datos);
+             $this->guardar_archivo($file, $parametros, $datos);
+             return 1;
+         } else {
+ 
+             $where = array(
+                 array('campo' => 'th_ctr_id', 'dato' => $txt_id_contratos_trabajos),
+             );
+ 
+             $datos = $this->modelo->editar($datos, $where);
+ 
+             if ($file['txt_ruta_archivo_contrato']['tmp_name'] != '' && $file['txt_ruta_archivo_contrato']['tmp_name'] != null) {
+                 $datos = $this->guardar_archivo($file, $parametros, $txt_id_contratos_trabajos);
+             }
+         }
+ 
+         return $datos;
+     }
+            
+ 
+ 
+     function eliminar($id)
+     {
+         $datos_archivo = $this->modelo->where('th_ctr_id', $id)->where('th_ctr_estado', 1)->listar();
+ 
+         if ($datos_archivo && isset($datos_archivo[0]['th_ctr_ruta_archivo'])) {
+             $ruta_relativa = ltrim($datos_archivo[0]['th_ctr_ruta_archivo'], './');
+             $ruta_archivo = dirname(__DIR__, 4) . '/' . $ruta_relativa;
+ 
+             if (file_exists($ruta_archivo)) {
+                 unlink($ruta_archivo);
+             }
+         }
+ 
+         $datos = array(
+             array('campo' => 'th_ctr_estado', 'dato' => 0),
+         );
+ 
+         $where = array(
+             array('campo' => 'th_ctr_id', 'dato' => strval($id)),
+         );
+ 
+ 
+         $datos = $this->modelo->editar($datos, $where);
+         return $datos;
+     }
+ 
+     private function guardar_archivo($file, $post, $id_insertar_editar)
+     {
+         $id_empresa = $_SESSION['INICIO']['ID_EMPRESA'];
+         $ruta = dirname(__DIR__, 4) . '/REPOSITORIO/TALENTO_HUMANO/' . $id_empresa . '/'; //ruta carpeta donde queremos copiar los archivos
+         $ruta .= $post['txt_postulante_cedula'] . '/' . 'CONTRATOS_TRABAJOS/';
+ 
+         if (!file_exists($ruta)) {
+             mkdir($ruta, 0777, true);
+         }
+ 
+         if ($this->validar_formato_archivo($file) === 1) {
+             $uploadfile_temporal = $file['txt_ruta_archivo_contrato']['tmp_name'];
+             $extension = pathinfo($file['txt_ruta_archivo_contrato']['name'], PATHINFO_EXTENSION);
+           
+             $nombre = 'contratos_trabajos_' . $id_insertar_editar . '.' . $extension;
+             $nuevo_nom = $ruta . $nombre;
+ 
+             $nombre_ruta = '../REPOSITORIO/TALENTO_HUMANO/' . $id_empresa . '/' . $post['txt_postulante_cedula'] . '/' . 'CONTRATOS_TRABAJOS/';
+             $nombre_ruta .= $nombre;
+             //print_r($post); exit(); die();
+ 
+             if (is_uploaded_file($uploadfile_temporal)) {
+                 if (move_uploaded_file($uploadfile_temporal, $nuevo_nom)) {
+ 
+                     $datos = array(
+                         array('campo' => 'th_ctr_ruta_archivo', 'dato' => $nombre_ruta),
+                     );
+ 
+                     $where = array(
+                         array('campo' => 'th_ctr_id', 'dato' => $id_insertar_editar),
+                     );
+ 
+                     // Ejecutar la actualización en la base de datos
+                     $base = $this->modelo->editar($datos, $where);
+ 
+                     return $base == 1 ? 1 : -1;
+                 } else {
+                     return -1;
+                 }
+             } else {
+                 return -1;
+             }
+         } else {
+             return -2;
+         }
+     }
+ 
+     private function validar_formato_archivo($file)
+     {
+         switch ($file['txt_ruta_archivo_contrato']['type']) {
+             case 'application/pdf':
+                 return 1;
+                 break;
+             default:
+                 return -1;
+                 break;
+         }
+     }
+ }
--- vista/PASANTES/02_ADRIAN/POSTULANTES/pos_contratos_trabajo.php
+++ vista/PASANTES/02_ADRIAN/POSTULANTES/pos_contratos_trabajo.php
@@ -1,29 +1,191 @@
 <script>
-    //Contratos de Trabajo
-    function insertar_editar_contrato_laboral() {
-        var txt_nombre_empresa_contrato = $('#txt_nombre_empresa_contrato').val();
-        var txt_copia_contrato = $('#txt_copia_contrato').val();
+  $(document).ready(function() {
+        <?php if (isset($_GET['id'])) { ?>
+            cargar_datos_contratos_trabajos(<?= $id ?>);
+        <?php } ?>
 
-        var parametros_contrato_laboral = {
-            'txt_nombre_empresa_contrato': txt_nombre_empresa_contrato,
-            'txt_copia_contrato': txt_copia_contrato,
+    });
+
+
+    //Contratos de Trabajo
+
+    function cargar_datos_contratos_trabajos(id) {
+        $.ajax({
+            url: '../controlador/PASANTES/02_ADRIAN/POSTULANTES/th_contratos_trabajosC.php?listar=true',
+            type: 'post',
+            data: {
+                id: id
+            },
+            dataType: 'json',
+            success: function(response) {
+                $('#pnl_contratos_trabajos').html(response);
+            }
+        });
+    }
+
+    function cargar_datos_modal_contratos_trabajos(id) {
+        $.ajax({
+            url: '../controlador/PASANTES/02_ADRIAN/POSTULANTES/th_contratos_trabajosC.php?listar_modal=true',
+            type: 'post',
+            data: {
+                id: id
+            },
+            dataType: 'json',
+            success: function(response) {
+                $('#txt_contratos_trabajos_id').val(response[0]._id);
+                $('#txt_ruta_guardada_contratos_trabajos').val(response[0].th_ctr_contratos_trabajos);
+                $('#txt_nombre_empresa_contrato').val(response[0].th_ctr_nombre_empresa);
+                $('#txt_tipo_contrato').val(response[0].th_ctr_tipo_contrato);
+                $('#txt_ruta_archivo_contrato').val(response[0].th_ctr_ruta_archivo);
+                $('#txt_fecha_inicio_contrato').val(response[0].th_ctr_fecha_inicio_contrato);
+                $('#txt_fecha_fin_contrato').val(response[0].th_ctr_fecha_fin_contrato);
+            }
+        });
+          
+    }
+
+    function insertar_editar_contratos_trabajos() {
+        var form_data = new FormData(document.getElementById("form_contratos_trabajos")); // Captura todos los campos y archivos
+        
+        var txt_id_contratos_trabajos = $('#txt_contratos_trabajos_id').val();
+        
+        if ($('#txt_ruta_archivo_contrato').val() === '' && txt_id_contratos_trabajos != '') {
+            var txt_ruta_archivo_contrato = $('#txt_ruta_guardada_contratos_trabajos').val()
+            $('#txt_ruta_archivo_contrato').rules("remove", "required");
+        } else {
+            var txt_ruta_archivo_contrato = $('#txt_ruta_archivo_contrato').val();
+            $('#txt_ruta_archivo_contrato').rules("add", {
+                required: true
+            });
         }
 
-        if ($("#form_contrato_trabajo").valid()) {
-            // Si es válido, puedes proceder a enviar los datos por AJAX
-            console.log(parametros_contrato_laboral)
+        // console.log([...form_data]);
+        // console.log([...form_data.keys()]);
+        // console.log([...form_data.values()]);
+        // return;
+
+        if ($("#form_contratos_trabajos").valid()) {
+
+            $.ajax({
+                url: '../controlador/PASANTES/02_ADRIAN/POSTULANTES/th_contratos_trabajosC.php?insertar=true',
+                type: 'post',
+                data: form_data,
+                contentType: false,
+                processData: false,
+
+                dataType: 'json',
+                success: function(response) {
+                    //console.log(response);
+                    if (response == -1) {
+                        Swal.fire({
+                            title: '',
+                            text: 'Algo extraño ha ocurrido, intente más tarde.',
+                            icon: 'error',
+                            allowOutsideClick: false,
+                            showConfirmButton: true,
+                            confirmButtonText: 'Cerrar'
+                        });
+                    } else if (response == -2) {
+                        Swal.fire({
+                            title: '',
+                            text: 'Asegúrese de que el archivo subido sea un PDF.',
+                            icon: 'error',
+                            allowOutsideClick: false,
+                            showConfirmButton: true,
+                            confirmButtonText: 'Cerrar'
+                        });
+                    } else if (response == 1) {
+                        Swal.fire('', 'Operación realizada con éxito.', 'success');
+                        <?php if (isset($_GET['id'])) { ?>
+                            cargar_datos_contratos_trabajos(<?= $id ?>);
+                        <?php } ?>
+                        limpiar_parametros_contratos_trabajos();
+                        $('#modal_agregar_contratos').modal('hide');
+                    }
+                }
+            });
         }
     }
+
+    
+    //Funcion para editar el registro de contratos y capacitaciones
+    function abrir_modal_contratos_trabajos(id) {
+        cargar_datos_modal_contratos_trabajos(id);
+
+        $('#modal_agregar_contratos').modal('show');
+
+        $('#lbl_titulo_contratos_trabajos').html('Editar Contrato');
+        $('#btn_guardar_contratos_trabajos').html('Guardar');
+
+    }
+
+    function delete_datos_contratos_trabajos() {
+        var id = $('#txt_contratos_trabajos_id').val();
+        Swal.fire({
+            title: 'Eliminar Registro?',
+            text: "Esta seguro de eliminar este registro?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si'
+        }).then((result) => {
+            if (result.value) {
+                eliminar_contratos_trabajos(id);
+            }
+        })
+    }
+
+    function eliminar_contratos_trabajos(id) {
+        $.ajax({
+            data: {
+                id: id
+            },
+            url: '../controlador/PASANTES/02_ADRIAN/POSTULANTES/th_contratos_trabajosC.php?eliminar=true',
+            type: 'post',
+            dataType: 'json',
+            success: function(response) {
+                if (response == 1) {
+                    Swal.fire('Eliminado!', 'Registro Eliminado.', 'success');
+                    <?php if (isset($_GET['id'])) { ?>
+                        cargar_datos_contratos_trabajos(<?= $id ?>);
+                    <?php } ?>
+                    limpiar_parametros_contratos_trabajos();
+                    $('#modal_agregar_contratos').modal('hide');
+                }
+            }
+        });
+    }
+
+    function limpiar_parametros_contratos_trabajos() {
+        //contratos capacitaciones
+        $('#txt_nombre_empresa_contrato').val('');
+        $('#txt_ruta_archivo_contrato').val('');
+        
+        $('#txt_contratos_trabajos_id').val('');
+        $('#txt_ruta_guardada_contratos_trabajos').val('');
+
+        //Limpiar validaciones
+        $("#form_contratos_trabajos").validate().resetForm();
+        $('.form-control').removeClass('is-valid is-invalid');
+
+        //Cambiar texto
+        $('#lbl_titulo_contratos_trabajos').html('Agregue un Contrato');
+        $('#btn_guardar_contratos_trabajos').html('Agregar');
+    }
+
+    function definir_ruta_iframe_contratos(url) {
+        var cambiar_ruta = $('#iframe_contratos_trabajos_pdf').attr('src', url);
+    }
+
+    function limpiar_parametros_iframe() {
+        $('#iframe_contratos_trabajos_pdf').attr('src', '');
+    }
+
 </script>
 
-<div class="row mb-2">
-    <div class="col-10 d-flex align-items-center">
-        <p class="fw-bold">Contrato de trabajo - Sambitours</p>
-    </div>
-    <div class="col-2 d-flex justify-content-end">
-        <a href="#" class=""><i class='text-info bx bx-download me-2' style="font-size: 20px;"></i></a>
-        <a href="#" class=""><i class='text-danger bx bx-trash me-0' style="font-size: 20px;"></i></a>
-    </div>
+<div id="pnl_contratos_trabajos"> 
+
 </div>
 
 <!-- Modal para agregar contratos de trabajo-->
@@ -33,44 +195,102 @@
 
             <!-- Modal Header -->
             <div class="modal-header">
-                <h5><small class="text-body-secondary">Agregue un Contrato</small></h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" onclick=""></button>
+                <h5><small class="text-body-secondary" id="lbl_titulo_contratos_trabajos">Agregue un Contrato de Trabajo</small></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" onclick="limpiar_parametros_contratos_trabajos()"></button>
             </div>
             <!-- Modal body -->
-            <form id="form_contrato_trabajo">
-                <div class="modal-body">
+            <form id="form_contratos_trabajos" enctype="multipart/form-data" method="post" style="width: inherit;">
+                
+            <div class="modal-body">
+
+                    <input type="hidden" name="txt_contratos_trabajos_id" id="txt_contratos_trabajos_id">
+                    <input type="hidden" name="txt_postulante_cedula" id="txt_postulante_cedula">
+                    <input type="hidden" name="txt_postulante_id" id="txt_postulante_id">
+
+
                     <div class="row mb-col">
                         <div class="col-md-12">
-                            <label for="txt_nombre_empresa_contrato" class="form-label form-label-sm">Nombre de la empresa <label style="color: red;">*</label></label>
+                            <label for="txt_nombre_empresa_contrato" class="form-label form-label-sm">Nombre de la empresa </label>
                             <input type="text" class="form-control form-control-sm" name="txt_nombre_empresa_contrato" id="txt_nombre_empresa_contrato" placeholder="Escriba el nombre de la empresa que emitió el contrato">
                         </div>
                     </div>
 
                     <div class="row mb-col">
                         <div class="col-md-12">
-                            <label for="txt_copia_contrato" class="form-label form-label-sm">Copia del contrato firmado <label style="color: red;">*</label></label>
-                            <input type="file" class="form-control form-control-sm" name="txt_copia_contrato" id="txt_copia_contrato" accept=".pdf">
+                            <label for="txt_tipo_contrato" class="form-label form-label-sm">Tipo de Contrato </label>
+                            <input type="text" class="form-control form-control-sm" name="txt_tipo_contrato" id="txt_tipo_contrato" placeholder="Escriba el tipo de contrato">
                         </div>
                     </div>
+                    
+                    <div class="row mb-col">
+                        <div class="col-md-12">
+                            <label for="txt_fecha_inicio_contrato" class="form-label form-label-sm">Fecha de Inicio Contrato </label>
+                            <input type="date" class="form-control form-control-sm no_caracteres" name="txt_fecha_inicio_contrato" id="txt_fecha_inicio_contrato" onchange="txt_fecha_inicio_contrato_1();">
+                        </div>
+                    </div>
+                    <div class="row mb-col">
+                        <div class="col-md-12">
+                            <label for="txt_fecha_fin_contrato" class="form-label form-label-sm">Fecha de Fin Contrato </label>
+                            <input type="date" class="form-control form-control-sm no_caracteres" name="txt_fecha_fin_contrato" id="txt_fecha_fin_contrato" onchange="txt_fecha_fin_contrato_1();">
+                        </div>
+                    </div>
+                    <div class="row mb-col">
+                        <div class="col md-12">
+                            <input type="checkbox" class="form-check-input" name="cbx_fecha_final_laboral" id="cbx_fecha_final_laboral" onchange="checkbox_actualidad_exp_prev();">
+                            <label for="cbx_fecha_final_laboral" class="form-label form-label-sm">Actualidad</label>
+                        </div>
+                    </div>
+
+                    <div class="row mb-col">
+                        <div class="col-md-12">
+                            <label for="txt_ruta_archivo_contrato" class="form-label form-label-sm">Copia del contrato firmado </label>
+                            <input type="file" class="form-control form-control-sm" name="txt_ruta_archivo_contrato" id="txt_ruta_archivo_contrato" accept=".pdf"  value="" placeholder="">                        
+                        </div>
+                    </div>
+
+
                 </div>
 
                 <div class="modal-footer d-flex justify-content-center">
-                    <button type="button" class="btn btn-success btn-sm" id="btn_guardar_contratos" onclick="insertar_editar_contrato_laboral();">Guardar Contrato de Trabajo</button>
+                    <button type="button" class="btn btn-success btn-sm px-4 m-1" id="btn_guardar_contratos_trabajos" onclick="insertar_editar_contratos_trabajos();">Guardar</button>
+                    <button type="button" class="btn btn-danger btn-sm px-4 m-1" id="btn_eliminar_contratos_trabajos" onclick="delete_datos_contratos_trabajos();">Eliminar</button>
                 </div>
             </form>
         </div>
     </div>
 </div>
 
-<script>
+<div class="modal" id="modal_ver_pdf_contratos" tabindex="-1" aria-modal="true" role="dialog" data-bs-backdrop="static" data-bs-keyboard="false">
+    <div class="modal-dialog modal-dialog-centered modal-xl">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h5><small class="text-body-secondary" id="lbl_titulo_contratos_trabajos">Visualizacion Documento</small></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" onclick="limpiar_parametros_iframe();"></button>
+            </div>
+            <!-- Modal body -->
+            <form id="form_contratos_trabajos">
+                <div class="modal-body d-flex justify-content-center">
+                    <iframe src='' id="iframe_contratos_trabajos_pdf" frameborder="0" width="900px" height="700px"></iframe>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script>    
     $(document).ready(function() {
+        agregar_asterisco_campo_obligatorio('txt_nombre_empresa_contrato');
+        agregar_asterisco_campo_obligatorio('txt_tipo_contrato');
+
         //Validación Contratos de Trabajo
-        $("#form_contrato_trabajo").validate({
+        $("#form_contratos_trabajos").validate({
             rules: {
                 txt_nombre_empresa_contrato: {
                     required: true,
                 },
-                txt_copia_contrato: {
+                txt_ruta_archivo_contrato: {
                     required: true,
                 },
             },
@@ -78,7 +298,7 @@
                 txt_nombre_empresa_contrato: {
                     required: "Por favor ingrese el nombre de la empresa",
                 },
-                txt_copia_contrato: {
+                txt_ruta_archivo_contrato: {
                     required: "Por favor suba la copia de su contrato",
                 },
             },
