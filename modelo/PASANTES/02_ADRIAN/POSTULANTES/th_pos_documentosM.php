f57941295f6b0d46b06a54777231e1c31c48772c
StalinVinueza <csvinueza@puce.edu.ec>
2024-11-22 11:21:35 -0500

    documentos identifiacion

--- modelo/PASANTES/02_ADRIAN/POSTULANTES/th_pos_documentosM.php
+++ modelo/PASANTES/02_ADRIAN/POSTULANTES/th_pos_documentosM.php
@@ -16,24 +16,6 @@ class th_pos_documentosM extends BaseModel
         'th_poi_fecha_modificacion',
         'th_poi_estado',
     ];
-
-
-    // function select_all(){
-    //     $sql = "SELECT * FROM th_pos_documentos";
-
-    //     $datos = $this->db->datos($sql);
-    //     return $datos;
-
-    // }
-
-    // CREATE TABLE th_pos_documentos (
-    //     th_poi_id INT IDENTITY(1,1) PRIMARY KEY,
-    //     th_pos_id INT,
-    //     th_poi_tipo VARCHAR(100),
-    //     th_poi_ruta_archivo VARCHAR(400),
-    //     th_poi_fecha_creacion DATETIME2 DEFAULT GETDATE(),
-    //     th_poi_fecha_modificacion DATETIME2,
-    //     th_poi_estado SMALLINT DEFAULT 1
-    // );
+   
 }
 
