9f718196b9489d25b2e513a1a417890d9c9f3c5d
javier farinango <javier.farinango92@gmail.com>
2025-01-28 12:04:30 -0500

    disenio etiqueta

--- vista/DISENIADOR_ZPL/DISENIADOR/di_diseniador.php
+++ vista/DISENIADOR_ZPL/DISENIADOR/di_diseniador.php
@@ -16,10 +16,12 @@
 <script language="javascript" type="text/javascript" src="../lib/ZPL/js/labelControls/LabelSize.js"></script>
 <script language="javascript" type="text/javascript" src="../lib/ZPL/js/labelControls/GenerateZPL.js"></script>
 <script language="javascript" type="text/javascript" src="../lib/ZPL/js/lindell-barcode/JsBarcode.all.min.js"></script>
+<script language="javascript" type="text/javascript" src="../lib/ZPL/js/lindell-barcode/qrcode.min.js"></script>
 <script language="javascript" type="text/javascript" src="../lib/ZPL/js/tools/rectangle.js"></script>
 <script language="javascript" type="text/javascript" src="../lib/ZPL/js/tools/barcode.js"></script>
 <script language="javascript" type="text/javascript" src="../lib/ZPL/js/tools/text.js"></script>
 <script language="javascript" type="text/javascript" src="../lib/ZPL/js/tools/image.js"></script>
+<script language="javascript" type="text/javascript" src="../lib/ZPL/js/tools/QRcode.js"></script>
 
 <script>
     var canvasDesigner = null;
@@ -34,6 +36,7 @@
         canvasDesigner.toolbar.addTool(new com.logicpartners.designerTools.rectangle());
         canvasDesigner.toolbar.addTool(new com.logicpartners.designerTools.barcode());
         canvasDesigner.toolbar.addTool(new com.logicpartners.designerTools.image());
+        canvasDesigner.toolbar.addTool(new com.logicpartners.designerTools.QRcode());
 
         // canvasDesigner.addRectangle(50, 50, 300, 50);
         // canvasDesigner.addRectangle(100, 100, 50, 50);
@@ -95,6 +98,21 @@
 
     }
 
+
+    function textos_iguales()
+    {
+       if($('#rbl_igual').prop('checked'))
+       {
+            texto = $('#text').val()
+            $('#txt_rfid').val(texto);
+
+       }else
+       {
+         $('#txt_rfid').val("Texto 1");
+       }
+
+    }
+
 </script>
 
 
@@ -126,39 +144,19 @@
                 <button class="btn-sm btn-success btn" onclick="imprimir()"><i class="bx bx-print"></i> Imprimir</button>
             </div>    
         </div>
+        <div class="row">
+            <div class="card mb-1">
+                <div class="card-body p-1">
+                    <div class="col-12" id="pnl_new_size">                                
+                    </div>   
+                </div>                
+            </div>                     
+        </div>
 
         <div class="row">
-            <div class="col-sm-9">
+             <div class="col-sm-3" > 
                 <div class="card">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-6" id="pnl_new_tools">
-                                
-                            </div> 
-                            <div class="col-6">
-                                <label><input type="checkbox" name="rbl_rfid" id="rbl_rfid"> RFID</label>
-                                <br>
-                                <b>Texto para RFID</b>
-                                <input type="" name="txt_rfid" id="txt_rfid" class="form-control form-control-sm">
-                            </div>                          
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <canvas id="labelDesigner" tabindex="1" width="800" height="900"
-                                style="border: 1px solid #000000;">
-                                </canvas>                                
-                            </div>
-                             <div class="col-12" id="pnl_new_size">
-                                
-                            </div>
-                            
-                        </div>
-                    </div>
-                </div>    
-            </div>
-            <div class="col-sm-3" > 
-                <div class="card">
-                    <div class="card-body">
+                    <div class="card-body p-0">
                         <div class="row" id="pnl_new_detalles">
 
                         </div>
@@ -167,7 +165,29 @@
                         </div>
                     </div>
                 </div>    
-            </div>            
+            </div>   
+            <div class="col-sm-9">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">                            
+                            <div class="col-12" id="pnl_new_tools">
+                                
+                            </div>
+                                                   
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <canvas id="labelDesigner" tabindex="1" width="800" height="900"
+                                style="border: 1px solid #000000;">
+                                </canvas>                                
+                            </div>
+                            
+                            
+                        </div>
+                    </div>
+                </div>    
+            </div>
+                    
         </div>
 
       
