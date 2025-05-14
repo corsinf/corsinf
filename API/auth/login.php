2523c850841b7fa75bde216c236866ccf5d58c7a
Your rubenpilca70 <rubenpilca70@gmail.com>
2025-05-14 12:04:40 -0500

    API validacion 2

--- API/auth/login.php
+++ API/auth/login.php
@@ -32,29 +32,33 @@ class Auth
         // $data->usuario = 'paco@pepe.com';
         // $data->empresa = 'woieruasdfhn123466';
 
-        $this->db->modificar_parametros_db($data->empresa);
+        // $this->db->modificar_parametros_db($data->empresa);
 
-        // Preparar la consulta para obtener el usuario
-        $sql = "SELECT TOP 1 id_usuarios, nombres, apellidos, password FROM usuarios WHERE email = '$data->usuario';";
-        $user = $this->db->datos($sql);
+        // // Preparar la consulta para obtener el usuario
+        // $sql = "SELECT TOP 1 id_usuarios, nombres, apellidos, password FROM usuarios WHERE email = '$data->usuario';";
+        // $user = $this->db->datos($sql);
 
 
 
-        if ($user && ($data->clave == $this->cod_global->desenciptar_clave($user[0]['password']))) {
+        // if ($user && ($data->clave == $this->cod_global->desenciptar_clave($user[0]['password']))) {
             // Generar el token JWT
-            $token = $this->jwtHandler->generateToken(
-                [
-                    'id' => $user[0]['id_usuarios'],
-                    'usuario' => $user[0]['nombres'],
+            // $token = $this->jwtHandler->generateToken(
+            //     [
+            //         'id' => $user[0]['id_usuarios'] ?? 'hola',
+            //         'usuario' => $user[0]['nombres'] ?? 'hola',
+            //         'empresa' => $data->empresa,
+            //     ]
+            // );
+            echo json_encode( [[
+                    'id' => $user[0]['id_usuarios'] ?? 'hola',
+                    'usuario' => $user[0]['nombres'] ?? 'hola',
                     'empresa' => $data->empresa,
-                ]
-            );
-            echo json_encode(["token" => $token]);
-        } else {
-            // Si las credenciales son incorrectas
-            http_response_code(401);
-            echo json_encode(["error" => "Credenciales inválidas"]);
-        }
+                ]], true);
+        // } else {
+        //     // Si las credenciales son incorrectas
+        //     http_response_code(401);
+        //     echo json_encode(["error" => "Credenciales inválidas"]);
+        // }
     }
 }
 
