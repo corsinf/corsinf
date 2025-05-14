c8ca883cc63d327101c396f815032c695ff71b7c
Your rubenpilca70 <rubenpilca70@gmail.com>
2025-05-14 12:43:17 -0500

    API validacion 3

--- API/index.php
+++ API/index.php
@@ -1,6 +1,17 @@
 <?php
 header('Content-Type: application/json');
 
+// CORS headers globales
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+// Responder a preflight
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
