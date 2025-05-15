dc96fba592c9cbafab078c0fbbeac95e17525947
Your rubenpilca70 <rubenpilca70@gmail.com>
2025-05-15 15:27:14 -0500

    quitar TinyMCE

--- cabeceras/footer3.php
+++ cabeceras/footer3.php
@@ -10,8 +10,7 @@
 	<script src="../assets/plugins/select2/js/select2.min.js"></script>
 	<script src="../assets/js/form-select2.js"></script>	
 
-	<script src='https://cdn.tiny.cloud/1/vdqx2klew412up5bcbpwivg1th6nrh3murc6maz8bukgos4v/tinymce/5/tinymce.min.js' referrerpolicy="origin">
-	</script>
+	<!-- <script src='https://cdn.tiny.cloud/1/vdqx2klew412up5bcbpwivg1th6nrh3murc6maz8bukgos4v/tinymce/5/tinymce.min.js' referrerpolicy="origin"></script> -->
 	<!-- <script src="../assets/js/form-text-editor.js"></script> -->
 	<script src="../assets/plugins/summernote/summernote-bs4.min.js"></script>
 	<!--app JS-->
