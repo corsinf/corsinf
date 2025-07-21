f91c5cf8d930c4c7ab9928af4abe9b992ca209c1
rubenpilca70 <rubenpilca70@gmail.com>
2025-07-21 13:13:50 -0500

    modelo triangulacion

--- modelo/TALENTO_HUMANO/th_triangular_departamento_personaM.php
+++ modelo/TALENTO_HUMANO/th_triangular_departamento_personaM.php
@@ -19,10 +19,53 @@ class th_triangular_departamento_personaM extends BaseModel
     function listarJoin()
     {
         $this->join('th_departamentos', 'th_departamentos.th_dep_id = th_triangular_departamento_persona.th_dep_id'); // ← corregido
-       
+
         $this->join('th_triangular', 'th_triangular.th_tri_id = th_triangular_departamento_persona.th_tri_id'); // ← corregido
 
         return $this->listar();
     }
-}
 
+    function validar_triangulacion($id_persona = '')
+    {
+        $sql =
+            "SELECT 
+                t.th_tri_id,
+                t.th_tri_nombre,
+                -- t.th_tri_descripcion,
+                tdp.th_per_id AS persona_origen,
+                'PERSONAL' AS origen,
+                ti.th_itr_id,
+                ti.th_itr_latitud,
+                ti.th_itr_longitud,
+                ti.th_itr_n_punto
+                -- ti.th_itr_fecha_creacion
+            FROM th_triangular_departamento_persona tdp
+            INNER JOIN th_triangular t ON t.th_tri_id = tdp.th_tri_id
+            LEFT JOIN th_triangular_item ti ON ti.th_tri_id = t.th_tri_id
+            WHERE tdp.th_per_id = $id_persona AND t.th_tri_estado = 1
+
+            UNION
+
+            SELECT 
+                t.th_tri_id,
+                t.th_tri_nombre,
+                -- t.th_tri_descripcion,
+                pd.th_per_id AS persona_origen,
+                'POR_DEPARTAMENTO' AS origen,
+                ti.th_itr_id,
+                ti.th_itr_latitud,
+                ti.th_itr_longitud,
+                ti.th_itr_n_punto
+                -- ti.th_itr_fecha_creacion
+            FROM th_triangular_departamento_persona tdp
+            INNER JOIN th_triangular t ON t.th_tri_id = tdp.th_tri_id
+            INNER JOIN th_personas_departamentos pd ON pd.th_dep_id = tdp.th_dep_id
+            LEFT JOIN th_triangular_item ti ON ti.th_tri_id = t.th_tri_id
+            WHERE pd.th_per_id = $id_persona AND t.th_tri_estado = 1;";
+
+        // print_r($sql); exit(); die();
+
+        $datos = $this->db->datos($sql);
+        return $datos;
+    }
+}
