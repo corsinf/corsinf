c7706e91499f9bde7818c715216557a2047a8541
rubenpilca70 <rubenpilca70@gmail.com>
2025-08-08 11:27:07 -0500

    feat: logs para el modulo de th cuando se recalcula las asistencias

--- Cron/TALENTO_HUMANO/DB/DB_SAINT_DOMINIC.php
+++ Cron/TALENTO_HUMANO/DB/DB_SAINT_DOMINIC.php
@@ -4,16 +4,25 @@ require_once('../calculo_control_acceso.php');
 // Crear una instancia de la clase y llamar al método
 $proceso = new calculo_persona($usuario, $password, $servidor, $database, $puerto);
 
-
 date_default_timezone_set('America/Mexico_City'); // Ajusta a tu zona horaria
-$fecha_actual = date("Y-m-d");
+// $fecha_actual = date("Y-m-d");
+$fecha_actual = '2025-08-07';
 
 // $parametros = $proceso->calculo_persona_control_acceso(2000, '2025-06-27');
 
-// $parametros = $proceso->carga_masiva('2025-07-14');
+guardar_log('[INF] Inicio Inserción Masiva ', $database);
 $parametros = $proceso->carga_masiva($fecha_actual);
+guardar_log($parametros, $database);
 
 print_r($parametros);
 exit();
-die();
- 
+
+function guardar_log($mensaje, $db)
+{
+    $ruta_log = __DIR__ . '/log_carga_masiva_' . $db . '.log';
+    $fecha_log = date("Y-m-d H:i:s");
+
+    $entrada = "[$fecha_log] $mensaje\n";
+
+    file_put_contents($ruta_log, $entrada, FILE_APPEND);
+}
