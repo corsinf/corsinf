3cd11d2cfda926c678a1ca3c0194bfc5587d6bd0
pilcaruben <rpilca@est.ups.edu.ec>
2025-10-27 10:19:08 -0500

    feat: marcaciones

--- modelo/TALENTO_HUMANO/th_cardM.php
+++ modelo/TALENTO_HUMANO/th_cardM.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 require_once(dirname(__DIR__, 1) . '/GENERAL/BaseModel.php');
 
 class th_cardM  extends BaseModel
 {
-	
-	protected $tabla = 'th_card_data';
+
+    protected $tabla = 'th_card_data';
     protected $primaryKey = 'th_card_id AS _id';
 
     protected $camposPermitidos = [
@@ -16,6 +16,18 @@ class th_cardM  extends BaseModel
         'th_card_creacion',
     ];
 
+    function sincronizar_datos_card_persona()
+    {
+        $sql =
+            "UPDATE c
+                SET c.th_per_id = p.th_per_id
+                FROM th_card_data AS c
+                JOIN th_personas AS p
+                ON LTRIM(RTRIM(p.th_per_observaciones)) = LTRIM(RTRIM(c.th_card_nombre))
+                WHERE c.th_per_id IS NULL;";
 
+
+        $datos = $this->db->sql_string($sql);
+        return $datos;
+    }
 }
-?>
--- modelo/TALENTO_HUMANO/th_personasM.php
+++ modelo/TALENTO_HUMANO/th_personasM.php
@@ -41,7 +41,7 @@ class th_personasM extends BaseModel
         //'th_per_fecha_modificacion AS fecha_modificacion',
         'PERFIL',
         //'PASS',
-        
+
         // Campos adicionales que pueden ser necesarios en el futuro
         // 'th_per_es_admin',
         // 'th_per_habiltado',
