87dbb1453dd77be28426b2150630fd579a96a96f
hgsvp-master <hgsvphospital@gmail.com>
2026-03-03 10:15:46 -0500

    merge: dispositivos

--- js/GENERAL/operaciones_generales.js
+++ js/GENERAL/operaciones_generales.js
@@ -420,7 +420,7 @@ function cargar_select2_url(ddl, url_controlador, placeholder = '-- Seleccione -
             },
             processResults: function (data) {
                 //console.log(data.length);
-                console.log(data);
+                //console.log(data);
                 return { results: data };
             },
             cache: true
@@ -580,8 +580,7 @@ function confirmar_terminos_datos(url) {
     });
 }
 
-function minutosAHora(minutos) 
-{
+function minutosAHora(minutos) {
     const horas = Math.floor(minutos / 60);
     const minutosRestantes = minutos % 60;
     // Formato HH:MM (con 2 dígitos)
