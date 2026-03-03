6ddd36edb9caa6a85636ecb409d6340a3f83462e
rubenpilca70 <rubenpilca70@gmail.com>
2026-03-03 11:38:49 -0500

    docs: pruebas

--- vista/TALENTO_HUMANO/CARGOS/th_organigrama.php
+++ vista/TALENTO_HUMANO/CARGOS/th_organigrama.php
@@ -1,2 +1,2 @@
 
-<?php include_once('../vista/TALENTO_HUMANO/CARGOS/MENU_ASPECTOS_INTRINSECOS/aspectos_intrinsecos.php'); ?> 
+<?php include_once('../vista/TALENTO_HUMANO/CARGOS/MENU_ASPECTOS_INTRINSECOS/aspectos_intrinsecos.php'); ?>
